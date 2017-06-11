c8f6bc9e78075ca3b8a45a31ca8694cab7a9fe6f
ramakellio <ramakellio@gmail.com>
2017-06-11 16:59:52 -0400

    Added junit test case

---
+++ src/notepadTest.java
@@ -0,0 +1,9 @@
+public class notepadTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+}
