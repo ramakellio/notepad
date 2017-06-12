e991a450ae8afdcd577c4d4915d1bad34a4eda7f
ramakellio <ramakellio@gmail.com>
2017-06-12 19:11:01 -0400

    Added system generated Junit test file

---
+++ src/notepadTestAuto.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author ramakellio
+ *
+ */
+public class notepadTestAuto {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link notepad#notepad()}.
+	 */
+	@Test
+	public void testNotepad() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link notepad#actionPerformed(java.awt.event.ActionEvent)}.
+	 */
+	@Test
+	public void testActionPerformed() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link notepad#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
